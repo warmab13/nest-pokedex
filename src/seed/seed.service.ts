1c0c1fb00014c19af0d81860d9c0a55ed04ac672
Alonso Salcido <warmab13@Alonsos-MacBook-Pro-2.local>
2023-10-17 01:32:59 -0600

    insert pokemon chunks making an axios request and make insertions to mongo db database using promises

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,24 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+import { CreatePokemonDto } from 'src/pokemon/dto/create-pokemon.dto';
 
 @Injectable()
 export class SeedService {
 
   private readonly axios:AxiosInstance = axios;
 
-
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
 
   async executeSeed() {
+    await this.pokemonModel.deleteMany({}); //delte * from pokemon 
+
     const { data } = await this.axios.get<PokeResponse>('http://pokeapi.co/api/v2/pokemon?limit=10');
+
+    const insertPromisesArray = [];
 
     data.results.forEach(({name, url})=>{
       const segments = url.split('/');
       const no:number = +segments[ segments.length - 2 ];
 
-      console.log({name, no})
+      //const pokemon = await this.pokemonModel.create( {name, no} );
+
+      insertPromisesArray.push(
+        this.pokemonModel.create({name, no})
+      )
+
     })
-    return data.results;
+    await Promise.all(insertPromisesArray);
+    return "Seed Executed"
   }
 
 }
